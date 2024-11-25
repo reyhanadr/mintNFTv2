fda78f087e9dbd2c46e5f491e3bd946e757515ad
reyhanadr <reyhan.a.deris@gmail.com>
2024-11-26 05:03:55 +0700

    fix layout gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -5,7 +5,7 @@ import { Flex, Text, Button } from '@/once-ui/components';
 import { Header } from "@/once-ui/modules/layout/Header";
 import { Footer } from "@/once-ui/modules/layout/Footer";
 import MasonryGrid from '@/components/MasonryGrid';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { ThirdwebProvider} from "@thirdweb-dev/react";
 import { Sepolia } from "@thirdweb-dev/chains";
 
@@ -37,9 +37,18 @@ export default function Gallery() {
 		<Flex fillWidth paddingTop="l" paddingX="l" direction="column" alignItems="center" flex={1}>
 		<Flex position="relative" as="section" overflow="hidden" fillWidth minHeight="0" direction="column" alignItems="center" flex={1}>
 			<Header/>
-			
 			<Flex gap="24" padding="24" alignItems="space-between" justifyContent="space-between" fillWidth >
-			<Text variant="heading-strong-l" onBackground="neutral-medium" marginTop="16">Gallery</Text>
+				<Text variant="heading-strong-l" onBackground="neutral-medium" marginTop="16">Gallery</Text>
+				{/* <Flex>
+				<Input
+					id="example"
+					label="Username"
+					value=""
+					hasPrefix={<Icon name="HiMiniMagnifyingGlass" />}
+					labelAsPlaceholder
+					// onChange={onChange}
+				/>
+				</Flex> */}
 			</Flex>
 
 			<Flex direction="column" gap="24" padding="24" alignItems="center" justifyContent="center" fillWidth radius="m" marginBottom='20'>
